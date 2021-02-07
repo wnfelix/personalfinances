a359cdddd8e23aa9b1e8b17286ffb3083b326b1a
William Felix <william.nfelix@gmail.com>
2021-02-07 17:43:39 -0300

    primeira versão oauth2

--- backend/FinancasPessoais/FinancasPessoais.WebAPI/App_Start/WebApiConfig.cs
+++ backend/FinancasPessoais/FinancasPessoais.WebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +9,7 @@ using System.Net.Http.Formatting;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
+//[assembly: OwinStartup(typeof(FinancasPessoais.WebAPI.WebApiConfig))]
 namespace FinancasPessoais.WebAPI
 {
     public static class WebApiConfig
@@ -16,7 +20,9 @@ namespace FinancasPessoais.WebAPI
              * olhar
              * https://docs.microsoft.com/pt-br/aspnet/web-api/overview/security/enabling-cross-origin-requests-in-web-api
              */
-
+            // Configure Web API to use only bearer token authentication.  
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 #if DEBUG
             // Web API configuration and services
             var cors = new EnableCorsAttribute("*", "*", "*");
---
+++ backend/FinancasPessoais/FinancasPessoais.WebAPI/Helpers/OauthTokenProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FinancasPessoais.WebAPI.Helpers
+{
+    public class OAuthTokenProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //if (FuncionariosSeguranca.Login(context.UserName, context.Password))
+            if (context.UserName == "william" && context.Password == "123")
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("acesso inválido", "As credenciais do usuário não conferem....");
+                return;
+            }
+        }
+    }
+}
